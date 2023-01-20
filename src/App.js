4e1eb4be91ff08bffa588d2b7b9d2515232d8e09
Abhishek Rathore <rathore.abhishek87@gmail.com>
2023-01-20 19:21:36 +0530

    react-1 udpated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let name  = 'react app'
+  let className = 'App-header';
+  console.log('App')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div className={className}>
+         {name}
+         <Demo></Demo>
+      </div>
     </div>
   );
 }
+function Demo() {
+  console.log('Demo')
+  return (
+    <div className="App">
+      <div className='App-header'>
+         Demo
+      </div>
+    </div>
+  );
+}
+
+
 
 export default App;
