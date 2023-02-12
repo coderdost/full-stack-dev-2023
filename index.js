907c26e3ad4a19ccdbd33ed972087377dd549ea2
Abhishek Rathore <rathore.abhishek87@gmail.com>
2023-02-13 03:34:15 +0530

    node-4

--- index.js
+++ index.js
@@ -9,49 +9,62 @@ const server = express();
 
 //bodyParser
 server.use(express.json());
-// server.use(express.urlencoded());
 server.use(morgan('default'))
 server.use(express.static('public'));
-// server.use((req, res, next) => {
-//   console.log(
-//     req.method,
-//     req.ip,
-//     req.hostname,
-//     new Date(),
-//     req.get('User-Agent')
-//   );
-//   next();
-// });
 
-const auth = (req, res, next) => {
-  // console.log(req.query);
 
-  // if (req.body.password == '123') {
-  //   next();
-  // } else {
-  //   res.sendStatus(401);
-  // }
-  next()
- 
-};
+
 
 // API - Endpoint - Route
-server.get('/product/:id', auth, (req, res) => {
-  console.log(req.params)
-  res.json({ type: 'GET' });
+
+// Products
+// API ROOT , base URL, example - google.com/api/v2/
+
+//Create POST /products     C R U D
+server.post('/products', (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.status(201).json(req.body);
 });
-server.post('/', auth, (req, res) => {
-  res.json({ type: 'POST' });
+
+
+
+// Read GET /products
+server.get('/products', (req, res) => {
+  res.json(products);
 });
-server.put('/', (req, res) => {
-  res.json({ type: 'PUT' });
+
+// Read GET /products/:id
+server.get('/products/:id', (req, res) => {
+  const id = +req.params.id;
+  const product = products.find(p=>p.id===id)
+  res.json(product);
 });
-server.delete('/', (req, res) => {
-  res.json({ type: 'DELETE' });
+
+// Update PUT /products/:id
+server.put('/products/:id', (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p=>p.id===id)
+  products.splice(productIndex,1,{...req.body, id:id})
+  res.status(201).json();
 });
-server.patch('/', (req, res) => {
-  res.json({ type: 'PATCH' });
+// Update PATCH /products/:id
+server.patch('/products/:id', (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p=>p.id===id)
+  const product = products[productIndex];
+  products.splice(productIndex,1,{...product,...req.body})
+  res.status(201).json();
 });
+// Delete DELETE /products/:id
+server.delete('/products/:id', (req, res) => {
+  const id = +req.params.id;
+  const productIndex = products.findIndex(p=>p.id===id)
+  const product = products[productIndex];
+  products.splice(productIndex,1)
+  res.status(201).json(product);
+});
+
 
 server.get('/demo', (req, res) => {
   // res.sendStatus(404);
