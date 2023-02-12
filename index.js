951832615b35553d87b0816269dc9e6b4b6807a9
Abhishek Rathore <rathore.abhishek87@gmail.com>
2023-02-13 02:55:30 +0530

    node-3

--- index.js
+++ index.js
@@ -1,55 +1,65 @@
-const http = require('http');
 const fs = require('fs');
-
 const index = fs.readFileSync('index.html', 'utf-8');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 const products = data.products;
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+const express = require('express');
+const morgan = require('morgan');
+const server = express();
 
-  if(req.url.startsWith('/product')){
-    const id = req.url.split('/')[2]
-    const product = products.find(p=>p.id===(+id))
-    console.log(product)
-    res.setHeader('Content-Type', 'text/html');
-          let modifiedIndex = index.replace('**title**', product.title)
-          .replace('**url**', product.thumbnail)
-          .replace('**price**', product.price)
-          .replace('**rating**', product.rating)
-          res.end(modifiedIndex);
-          return;
-  }
-//   '/product':
-//       res.setHeader('Content-Type', 'text/html');
-//       let modifiedIndex = index.replace('**title**', product.title)
-//       .replace('**url**', product.thumbnail)
-//       .replace('**price**', product.price)
-//       .replace('**rating**', product.rating)
-//       res.end(modifiedIndex);
-//       break;
+//bodyParser
+server.use(express.json());
+// server.use(express.urlencoded());
+server.use(morgan('default'))
+server.use(express.static('public'));
+// server.use((req, res, next) => {
+//   console.log(
+//     req.method,
+//     req.ip,
+//     req.hostname,
+//     new Date(),
+//     req.get('User-Agent')
+//   );
+//   next();
+// });
 
+const auth = (req, res, next) => {
+  // console.log(req.query);
 
+  // if (req.body.password == '123') {
+  //   next();
+  // } else {
+  //   res.sendStatus(401);
+  // }
+  next()
+ 
+};
 
-  switch (req.url) {
-    case '/':
-      res.setHeader('Content-Type', 'text/html');
-      res.end(index);
-      break;
-    case '/api':
-      res.setHeader('Content-Type', 'application/json');
-      res.end(JSON.stringify(data));
-      break;
-
-    default:
-      res.writeHead(404);
-      res.end();
-  }
-
-  console.log('server started  ');
-  //   res.setHeader('Dummy', 'DummyValue');
-
-  //
+// API - Endpoint - Route
+server.get('/product/:id', auth, (req, res) => {
+  console.log(req.params)
+  res.json({ type: 'GET' });
+});
+server.post('/', auth, (req, res) => {
+  res.json({ type: 'POST' });
+});
+server.put('/', (req, res) => {
+  res.json({ type: 'PUT' });
+});
+server.delete('/', (req, res) => {
+  res.json({ type: 'DELETE' });
+});
+server.patch('/', (req, res) => {
+  res.json({ type: 'PATCH' });
 });
 
-server.listen(8080);
+server.get('/demo', (req, res) => {
+  // res.sendStatus(404);
+  // res.json(products)
+  // res.status(201).send('<h1>hello</h1>')
+  // res.sendFile('/Users/abhishekrathore/Desktop/node-app/index.html')
+});
+
+server.listen(8080, () => {
+  console.log('server started');
+});
