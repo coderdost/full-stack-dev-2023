963fd9b42ed7d5165f1dc71bc795c014dc24db39
Abhishek Rathore <rathore.abhishek87@gmail.com>
2023-02-15 04:15:02 +0530

    node-8.2

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const server = express();
+const path = require('path');
 const productRouter = require('./routes/product')
 const userRouter = require('./routes/user')
 console.log('env',process.env.DB_PASSWORD)
@@ -28,9 +29,12 @@ async function main() {
 server.use(cors());
 server.use(express.json());
 server.use(morgan('default'));
-server.use(express.static(process.env.PUBLIC_DIR));
+server.use(express.static(path.resolve(__dirname,process.env.PUBLIC_DIR)));
 server.use('/products',productRouter.router);
 server.use('/users',userRouter.router);
+server.use('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'build','index.html'))
+})
 
 
 
