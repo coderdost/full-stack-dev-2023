2075dc6e63e88036a3eac87da42ba60c5b0b329d
Abhishek Rathore <rathore.abhishek87@gmail.com>
2023-02-14 21:31:00 +0530

    node-6

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const server = express();
 const productRouter = require('./routes/product')
 const userRouter = require('./routes/user')
-
+console.log('env',process.env.DB_PASSWORD)
 //bodyParser
 server.use(express.json());
 server.use(morgan('default'));
-server.use(express.static('public'));
+server.use(express.static(process.env.PUBLIC_DIR));
 server.use('/products',productRouter.router);
 server.use('/users',userRouter.router);
 
-server.listen(8080, () => {
+server.listen(process.env.PORT, () => {
   console.log('server started');
 });
