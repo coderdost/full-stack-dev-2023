25fddc3c521db5565974fe78be31fe71ce0dda89
Abhishek Goud <gaudabhishek260@gmail.com>
2023-12-20 14:02:36 +0530

    node-8

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/ecommerce');
   console.log('database connected')
 }
+// main().catch(err => console.log(err));
 //Schema
 
 
